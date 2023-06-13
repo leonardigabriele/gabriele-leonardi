d4f49bc7bcc24801de6161e5a8e910a9992571f4
Gabriele Leonardi <leonardi.gabriele.lg@gmail.com>
2023-06-13 18:15:59 +0200

    1.0

--- generate.mjs
+++ generate.mjs
@@ -1,27 +1,16 @@
-import projects from "./projects.json" assert { type: "json" };
+import projectsData from "./data/projects.json" assert { type: "json" };
+import contactsData from "./data/contacts.json" assert { type: "json" };
 import fs from "fs";
 
-const part1 = `<!DOCTYPE html>
+const header = `<!DOCTYPE html>
 <html>
-<head>
-<title>‎</title>
-<link rel="stylesheet" href="index.css" />
-</head>
-<body>
-<!-- ▒ p ▒ r ▒ o ▒ j ▒ e ▒ c ▒ t ▒ s ▒ -->`;
-
-const part2 = `<!--------------------------------------->
-<instructions>
-<windows>
-⊞ windows: <kbd>ctrl</kbd> + <kbd>shift</kbd> + <kbd>c</kbd>
-</windows>
-<mac> ⌘ mac: <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>c</kbd> </mac>
-<linux>
-◷ linux: <kbd>ctrl</kbd> + <kbd>shift</kbd> + <kbd>c</kbd>
-</linux>
-</instructions>
-</body>
-</html>`;
+  <head>
+    <title>‎</title>
+    <link rel="stylesheet" href="index.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>`;
 
 const states = new Map([
   ["wip", "○"],
@@ -29,18 +18,73 @@ const states = new Map([
   ["archived", "□"],
 ]);
 
-fs.writeFile(
-  "index.html",
-  `${part1}${Object.entries(projects).reduce(
-    (a, [k, v]) => `${a}
-<${k} ✣="${v.tagline}">
-<state ${states.get(v.state)}="${v.state}"></state>
-<project name="${v.project}" year="${v.year}"></project>
-<a ctrl\\cmd+click="→" href="${v.link}"></a>
-<stack backend="${v.backend}" api="${v.api}" frontend="${v.frontend}"></stack>
-</${k}>`,
-    ""
-  )}
-  ${part2}`,
-  () => {}
-);
+const projects = `
+
+    <!-- ▒ p ▒ r ▒ o ▒ j ▒ e ▒ c ▒ t ▒ s ▒ -->
+${Object.entries(projectsData).reduce(
+  (a, [k, v]) => `${a}
+    <${k} ✣="${v.tagline}">
+      <state ${states.get(v.state)}="${v.state}"></state>
+      <project name="${v.project}" year="${v.year}"></project>
+      <a ctrl\\cmd+click="→" href="${v.link}"></a>
+      <stack backend="${v.backend}" api="${v.api}" frontend="${
+    v.frontend
+  }"></stack>
+    </${k}>
+    `,
+  ""
+)}`;
+
+const contacts = `
+    <!-- ▒ c ▒ o ▒ n ▒ t ▒ a ▒ c ▒ t ▒ s ▒ -->
+
+    <profile name="${contactsData.name} ${contactsData.surname}" role="${contactsData.role}"></profile>
+    <a ctrl\\cmd+click="→" href="mailto:${contactsData.email}"></a>
+    <a ctrl\\cmd+click="→" href="tel:${contactsData.phone}"></a>
+    <a ctrl\\cmd+click="→" href="${contactsData.linkedin}"></a>
+`;
+
+const pageHeader = `
+<!---->
+<page>
+<instructions>
+<keyboard-shortcuts>
+<windows>⊞ Windows: <kbd>ctrl</kbd> + <kbd>shift</kbd> + <kbd>c</kbd></windows>
+<mac>⌘ Mac: <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>c</kbd></mac>
+<linux>◷ Linux: <kbd>ctrl</kbd> + <kbd>shift</kbd> + <kbd>c</kbd></linux>
+</keyboard-shortcuts>
+<a href="#inspector">No keyboard?</a>
+</instructions>
+<inspector id="inspector">
+<nav>
+<div>Inspector</div>
+<a href="#index">Close</a>
+</nav>
+<pre>`;
+
+const pageFooter = `
+</pre>
+</inspector>
+</page>`;
+
+const footer = `
+  </body>
+</html>`;
+
+const escaped = (content) =>
+  content.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+
+const parts = [
+  header,
+  projects,
+  contacts,
+  pageHeader,
+  escaped(header),
+  escaped(projects),
+  escaped(contacts),
+  escaped(footer),
+  pageFooter,
+  footer,
+];
+
+fs.writeFile("index.html", parts.join(""), () => {});
